f6a34683831b6fb791a33c73ec04ec041e04e0b2
LadyMiteSolano <56986716+LadyMiteSolano@users.noreply.github.com>
2023-07-18 13:38:47 -0500

    login.js

---
+++ Teka/src/main/java/com/example/Teka/TekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Teka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekaApplication.class, args);
+	}
+
+}
---
+++ Teka/src/main/resources/js/login.js
@@ -0,0 +1,18 @@
+function validarInicioSesion() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if (username === 'admin' && password === '1234') {
+        // Redireccionar a la página de inicio.html si el inicio de sesión es exitoso
+        window.location.href = 'menu.html';
+        return false;
+    } else {
+        alert('Usuario o contraseña incorrectos.');
+        return false;
+    }
+}
+
+function registrarse() {
+    // Aquí puedes agregar la lógica para redirigir a la página de registro si es necesario.
+    // Por ejemplo: window.location.href = 'registro.html';
+}
