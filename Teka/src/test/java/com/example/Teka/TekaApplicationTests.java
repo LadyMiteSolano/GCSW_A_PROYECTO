e162d6a1ce7b68399d766a7342073469f8727f12
LadyMiteSolano <56986716+LadyMiteSolano@users.noreply.github.com>
2023-07-18 13:38:47 -0500

    login

---
+++ Teka/src/test/java/com/example/Teka/TekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Teka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
